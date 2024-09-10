4c28830dbdc7c50563ab6c1566432f9f807cbce9
ShahinALomShuvo <shahinalomshuvo94@gmail.com>
2024-09-10 22:07:33 +0600

    Implement Car Model

---
+++ src/modules/car/car.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+import { TCar } from "./car.interface";
+
+const CarSchema = new Schema<TCar>(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    color: { type: String, required: true },
+    isElectric: { type: Boolean, required: true },
+    status: {
+      type: String,
+      enum: ["available", "unavailable"],
+      default: "available",
+    },
+    features: { type: [String], required: true },
+    pricePerHour: { type: Number, required: true },
+    isDeleted: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Car = mongoose.model<TCar>("Car", CarSchema);
+
+export default Car;
