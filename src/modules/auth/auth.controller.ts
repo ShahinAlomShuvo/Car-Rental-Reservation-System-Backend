2fd092cdab3d2ebba46177775c4168e886e2f10a
ShahinALomShuvo <shahinalomshuvo94@gmail.com>
2024-09-10 16:33:48 +0600

    Implement sendResponse utilis

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import { authService } from "./auth.service";
 import { userValidation } from "../user/user.validation";
 import catchAsync from "../../utils/catchAsync.utils";
+import sendResponse from "../../utils/sendResponse.utils";
+import httpStatus from "http-status";
 
 const signUp = catchAsync(async (req, res) => {
   const userData = req.body;
   const parsedData = userValidation.userValidationSchema.parse(userData);
   const user = await authService.signUp(parsedData);
-  res.status(201).json({
-    status: "success",
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User registered successfully",
     data: user,
   });
 });
