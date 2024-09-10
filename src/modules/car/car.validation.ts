20fdbc714c26c8edb01d9958fd2894a1ef2558d4
ShahinALomShuvo <shahinalomshuvo94@gmail.com>
2024-09-10 22:14:18 +0600

    Implement Car Validation

---
+++ src/modules/car/car.validation.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const createCarValidationSchema = z.object({
+  name: z.string().min(1, "Car name is required"),
+  description: z.string().min(1, "Description is required"),
+  color: z.string().min(1, "Color is required"),
+  isElectric: z.boolean(),
+  status: z.enum(["available", "unavailable"]).default("available"),
+  features: z.array(z.string()).nonempty("At least one feature is required"),
+  pricePerHour: z.number().positive("Price per hour must be a positive number"),
+});
+
+const updateCarValidationSchema = createCarValidationSchema.partial();
+
+export const carValidation = {
+  createCarValidationSchema,
+  updateCarValidationSchema,
+};
