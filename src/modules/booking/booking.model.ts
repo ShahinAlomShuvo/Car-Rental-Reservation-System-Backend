a5d03cf8446d489fd22ee05a252b3505f52e44c6
ShahinALomShuvo <shahinalomshuvo94@gmail.com>
2024-09-11 16:30:35 +0600

    implement booking model

---
+++ src/modules/booking/booking.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import { TBooking } from "./booking.interface";
+
+const bookingSchema = new Schema<TBooking>({
+  date: {
+    type: Date,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  car: {
+    type: Schema.Types.ObjectId,
+    ref: "Car",
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    default: null,
+  },
+  totalCost: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Booking = model<TBooking>("Booking", bookingSchema);
+
+export default Booking;
